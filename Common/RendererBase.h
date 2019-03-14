24af6360ad7225f4a80cc05af0957b100525ce1e
Dongmiao Liu <b8052301@newcastle.ac.uk>
2019-03-14 16:07:29 +0000

    /

--- Common/RendererBase.h
+++ Common/RendererBase.h
@@ -22,24 +22,24 @@ namespace NCL {
 			RendererBase(Window& w);
 			virtual ~RendererBase();
 
-			virtual bool HasInitialised() const { return true; }
+			virtual bool HasInitialised() const {return true;}
 
 			virtual void Update(float dt) {}
 
-			void Render(float x, float y) {
+			void Render() {
 				BeginFrame();
-				RenderFrame(x, y);
+				RenderFrame();
 				EndFrame();
 			}
 
 		protected:
 			virtual void OnWindowResize(int w, int h) = 0;
 			virtual void OnWindowDetach() {}; //Most renderers won't care about this
-
-			virtual void BeginFrame() = 0;
-			virtual void RenderFrame(float x, float y) = 0;
-			virtual void EndFrame() = 0;
-
+			
+			virtual void BeginFrame()	= 0;
+			virtual void RenderFrame()	= 0;
+			virtual void EndFrame()		= 0;
+			
 			Window& hostWindow;
 
 			int currentWidth;
--- Plugins/PlayStation4/InputBase.cpp
+++ Plugins/PlayStation4/InputBase.cpp
@@ -22,12 +22,6 @@ void InputBase::ResetInput() {
 	}
 }
 
-// Axis 0 Left Joystick
-// Axis 1 Right Joystick
-// Axis 2 Directional buttons
-// + Right/Up   - Left/Down
-// x L/R        y Up/Down
-// [-1,1]
 AXIS	InputBase::GetAxis(unsigned int i) {
 	if (i > MAX_AXIS) {
 		return AXIS();
@@ -35,9 +29,6 @@ AXIS	InputBase::GetAxis(unsigned int i) {
 	return axis[i];
 }
 
-//Button 0 Triangle Button 1 Circle Button 2 Cross Button 3 Square
-//Button 4 L2       Button 5 R2     Button 6 L1    Button 7 L2
-//Button 8 L3       Button 9 R3
 float	InputBase::GetButton(unsigned int i) {
 	if (i > MAX_BUTTONS) {
 		return 0.0f;
