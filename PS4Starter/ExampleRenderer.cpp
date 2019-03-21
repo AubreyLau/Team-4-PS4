409603a966af3c64044044a614894a1176a0abc3
Dongmiao Liu <b8052301@newcastle.ac.uk>
2019-03-21 21:14:22 +0000

    objLoader

--- PS4Starter/ExampleRenderer.cpp
+++ PS4Starter/ExampleRenderer.cpp
@@ -17,7 +17,7 @@ ExampleRenderer::ExampleRenderer(PS4Window* window) : PS4RendererBase(window)
 	skybox[3] = new RenderObject(skyboxMeshFront, (ShaderBase*)skyboxShader, (TextureBase*)SkyboxTextureFront);
 	skybox[4] = new RenderObject(skyboxMeshUp, (ShaderBase*)skyboxShader, (TextureBase*)SkyboxTextureUp);
 	skybox[5] = new RenderObject(skyboxMeshDown, (ShaderBase*)skyboxShader, (TextureBase*)SkyboxTextureDown);
-	ball = new RenderObject((MeshGeometry*)setMesh("/app0/sphere2.msh"), (ShaderBase*)skyboxShader, (TextureBase*)myTexture);
+	ball = new RenderObject((MeshGeometry*)setMesh("/app0/sphere.obj"), (ShaderBase*)skyboxShader, (TextureBase*)myTexture);
 	floor = new RenderObject(floorMesh, (ShaderBase*)skyboxShader, (TextureBase*)floorTexture);
 	changePos = Vector3(0,0,0);
 	/*Mo test- skybox*/
